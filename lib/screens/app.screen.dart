5dc69d871213be43a768d8551b2dfea263d33d6e
maksim.kutprot@daubit.org <maksim.kutprot@daubit.org>
2022-10-05 16:59:11 +0200

    neu navbar-menu

--- lib/screens/app.screen.dart
+++ lib/screens/app.screen.dart
@@ -8,8 +8,10 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:open_store/open_store.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../injection/injector.dart';
 import '../models/podcasts/episode.model.dart';
@@ -43,7 +45,6 @@ class _AppScreenState extends State<AppScreen> {
   int currentTabIndex = 0;
   Episode? episode;
   final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
-
 
   Episode? setEpisode(Episode episode) {
     this.episode = episode;
@@ -89,35 +90,43 @@ class _AppScreenState extends State<AppScreen> {
 
   checkUpdates(context) async {
     final remoteConfig = FirebaseRemoteConfig.instance;
-    await remoteConfig.setDefaults(const {"iosBuildNumber": 1, "androidBuildNumber": 1 });
+    await remoteConfig
+        .setDefaults(const {"iosBuildNumber": 1, "androidBuildNumber": 1});
     await remoteConfig.setConfigSettings(RemoteConfigSettings(
       fetchTimeout: const Duration(minutes: 1),
       minimumFetchInterval: const Duration(hours: 1),
     ));
     await remoteConfig.fetchAndActivate();
 
-    final requiredBuildNumber = remoteConfig.getInt(Platform.isAndroid
-        ? 'androidBuildNumber'
-        : 'iosBuildNumber');
+    final requiredBuildNumber = remoteConfig
+        .getInt(Platform.isAndroid ? 'androidBuildNumber' : 'iosBuildNumber');
     var packageInfo = await PackageInfo.fromPlatform();
     final currentBuildNumber = int.parse(packageInfo.buildNumber);
-    if(currentBuildNumber < requiredBuildNumber) {
-      showDialog(context: context, builder: (_) => AlertDialog(
-        title: const Text("Update required"),
-        content: const Text("Please update your app to continue"),
-        elevation: 8,
-        actions: [
-          TextButton(onPressed: () {
-            OpenStore.instance.open(
-              appStoreId: '1637833839', // AppStore id of your app
-              androidAppBundleId: 'com.aboat.talkaboat', // Android app bundle package name
-            );
-          }, child: Text("Update")),
-          TextButton(onPressed: () {
-            Navigator.of(context).pop();
-          }, child: Text("Later"))
-        ],
-      ), barrierDismissible: true);
+    if (currentBuildNumber < requiredBuildNumber) {
+      showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+                title: const Text("Update required"),
+                content: const Text("Please update your app to continue"),
+                elevation: 8,
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        OpenStore.instance.open(
+                          appStoreId: '1637833839', // AppStore id of your app
+                          androidAppBundleId:
+                              'com.aboat.talkaboat', // Android app bundle package name
+                        );
+                      },
+                      child: Text("Update")),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Later"))
+                ],
+              ),
+          barrierDismissible: true);
     }
   }
 
@@ -155,38 +164,116 @@ class _AppScreenState extends State<AppScreen> {
               _buildOffstageNavigator("Library"),
               _buildOffstageNavigator("Social"),
             ]),
-            bottomNavigationBar:
-                Column(mainAxisSize: MainAxisSize.min, children: [
-              MiniPlayerWidget(episode: episode),
-              CurvedNavigationBar(
-                  backgroundColor: episode == null
-                      ? Colors.transparent
-                      : Theme.of(context).bottomAppBarColor,
-                  color: Theme.of(context)
-                      .bottomNavigationBarTheme
-                      .backgroundColor!,
-                  buttonBackgroundColor: Theme.of(context)
-                      .bottomNavigationBarTheme
-                      .selectedItemColor,
-                  animationCurve: Curves.easeInOut,
-                  animationDuration: const Duration(milliseconds: 600),
-                  height: Platform.isIOS ? 65 : 50,
-                  index: currentTabIndex,
-                  onTap: (index) {
-                    _selectTab(pageKeys[index], index);
-                  },
-                  items: <Widget>[
-                    Icon(Icons.home, size: 30),
-                    Icon(Icons.search, size: 30),
-                    Icon(Icons.playlist_add, size: 30),
-                    Stack(children: [
-                      SizedBox(height: 30, width: 30),
-                      Center(child: Icon(Icons.library_books, size: 30)),
-                      pageKeys[currentTabIndex] == "Library" || !userService.unseenLibraryNotifcationUpdates() ? SizedBox() :Positioned(right: 10, top:10, child: Icon(Icons.notifications_active, size: 20, color: Colors.red)),
-                    ],),
-                    Icon(Icons.people, size: 30)
-                  ]),
-            ])),
+            bottomNavigationBar: Container(
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+                MiniPlayerWidget(episode: episode),
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
+                  ),
+                  child: BottomNavigationBar(
+                    type: BottomNavigationBarType.fixed,
+                    selectedFontSize: 0,
+                    unselectedFontSize: 0,
+                    elevation: 0,
+                    showSelectedLabels: false,
+                    showUnselectedLabels: false,
+                    currentIndex: currentTabIndex,
+                    onTap: (index) {
+                      _selectTab(pageKeys[index], index);
+                    },
+                    items: <BottomNavigationBarItem>[
+                      BottomNavigationBarItem(
+                          icon: buttonNavbar(
+                              "assets/images/home.png", 20, 20, "Home"),
+                          activeIcon: buttonNavbarActiv(
+                              "assets/images/home.png", 20, 20, "Home"),
+                          label: ''),
+                      BottomNavigationBarItem(
+                          icon: buttonNavbar(
+                              "assets/images/live.png", 29, 20, "Live"),
+                          activeIcon: buttonNavbarActiv(
+                              "assets/images/live.png", 29, 20, "Live"),
+                          label: ''),
+                      BottomNavigationBarItem(
+                          icon: buttonNavbar("assets/images/favorites.png", 20,
+                              20, "Favorites"),
+                          activeIcon: buttonNavbarActiv(
+                              "assets/images/favorites.png",
+                              20,
+                              20,
+                              "Favorites"),
+                          label: ''),
+                      BottomNavigationBarItem(
+                          icon: buttonNavbar("assets/images/playlist.png", 30,
+                              20, "Playlists"),
+                          activeIcon: buttonNavbarActiv(
+                              "assets/images/playlist.png",
+                              30,
+                              20,
+                              "Playlists"),
+                          label: ''),
+                      BottomNavigationBarItem(
+                          icon: buttonNavbar(
+                              "assets/images/social.png", 20, 20, "Social"),
+                          activeIcon: buttonNavbarActiv(
+                              "assets/images/social.png", 20, 20, "Social"),
+                          label: ''),
+                    ],
+                  ),
+                )
+              ]),
+            )),
+      ),
+    );
+  }
+
+  Widget buttonNavbar(String image, double width, double height, String text) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            image,
+            width: width,
+            height: height,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 5),
+          Text(
+            text,
+            style: GoogleFonts.inter(textStyle: TextStyle(fontSize: 12)),
+          ),
+        ]);
+  }
+
+  Widget buttonNavbarActiv(
+      String image, double width, double height, String text) {
+    return Container(
+      height: 55,
+      width: 60,
+      decoration: const BoxDecoration(
+          color: Color.fromRGBO(114, 113, 113, 0.2),
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20))),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            image,
+            width: width,
+            height: height,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 5),
+          Text(
+            text,
+            style: GoogleFonts.inter(textStyle: const TextStyle(fontSize: 12)),
+          ),
+        ],
       ),
     );
   }
