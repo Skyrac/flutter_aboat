37930d4e4cf7ca0535d3ca1a4ba69b7d172ede3b
Alexander Krahl <alexander.krahl@daubit.org>
2022-10-06 16:55:43 +0200

    fix inkwell location

--- lib/widgets/home-categories.dart
+++ lib/widgets/home-categories.dart
@@ -30,33 +30,30 @@ class _HomeScreenCategoriesTabState extends State<HomeScreenCategoriesTab> {
   ];
 
   Widget buildCategoryList(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: GridView.count(
-            shrinkWrap: true,
-            crossAxisCount: 2,
-            semanticChildCount: data.length,
-            childAspectRatio: 170 / 70,
-            physics: const NeverScrollableScrollPhysics(),
-            children: List.generate(data.length, (index) {
-              if (data[index] == null) {
-                return const ListTile();
-              } else {
-                final item = data[index]!;
-                return makeCard(context, item, index);
-              }
-            })));
+    return GridView.count(
+        shrinkWrap: true,
+        crossAxisCount: 2,
+        semanticChildCount: data.length,
+        childAspectRatio: 170 / 70,
+        physics: const NeverScrollableScrollPhysics(),
+        children: List.generate(data.length, (index) {
+          if (data[index] == null) {
+            return const ListTile();
+          } else {
+            final item = data[index]!;
+            return makeCard(context, item, index);
+          }
+        }));
   }
 
   Widget makeCard(BuildContext context, PodcastCategory category, int index) {
     return Padding(
-        padding: EdgeInsets.only(bottom: 10, left: index / 2.0 == 0.5 ? 5 : 0, right: index / 2.0 == 0.0 ? 5 : 0),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: Container(
-            color: const Color.fromRGBO(99, 163, 253, 0.5),
-            child: Center(
-                child: InkWell(
+        padding: const EdgeInsets.all(10),
+        child: SizedBox(
+          width: 170,
+          height: 70,
+          child: InkWell(
+              borderRadius: BorderRadius.circular(10),
               onTap: () async {
                 /*await userService.UpdatePodcastVisitDate(entry.id);
             setState(() {});
@@ -70,34 +67,34 @@ class _HomeScreenCategoriesTabState extends State<HomeScreenCategoriesTab> {
                     reverseDuration: const Duration(milliseconds: 500),
                     child: PodcastDetailScreen(podcastSearchResult: entry)));*/
               },
-              child: SizedBox(
-                  width: 170,
-                  height: 70,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Center(
-                          child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: SizedBox(
-                                height: 90,
-                                width: 90,
-                                child: Image(
-                                  image: category.image,
-                                  fit: BoxFit.cover,
-                                ),
-                              ))),
-                      Center(
-                          child: Padding(
-                              padding: const EdgeInsets.only(top: 10),
-                              child: Text(category.name,
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 2,
-                                  style: Theme.of(context).textTheme.titleMedium)))
-                    ],
-                  )),
-            )),
-          ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                    color: const Color.fromRGBO(99, 163, 253, 0.5),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: SizedBox(
+                                  height: 50,
+                                  width: 50,
+                                  child: Image(
+                                    image: category.image,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ))),
+                        Center(
+                            child: Padding(
+                                padding: const EdgeInsets.only(left: 10),
+                                child: Text(category.name,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 2,
+                                    style: Theme.of(context).textTheme.labelLarge)))
+                      ],
+                    )),
+              )),
         ));
   }
 }
