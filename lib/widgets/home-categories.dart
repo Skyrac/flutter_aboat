80eaf55cb92f1237f7dbda3aadd8df0f9fde59d1
Alexander Krahl <alexander.krahl@daubit.org>
2022-10-07 16:12:29 +0200

    add categories, filter categories depending on search

--- lib/widgets/home-categories.dart
+++ lib/widgets/home-categories.dart
@@ -23,6 +23,9 @@ class _HomeScreenCategoriesTabState extends State<HomeScreenCategoriesTab> {
     ));
   }
 
+  //TextEditingController searchController = TextEditingController();
+  String search = '';
+
   Widget buildSearchField(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 10),
@@ -36,6 +39,12 @@ class _HomeScreenCategoriesTabState extends State<HomeScreenCategoriesTab> {
                     padding: const EdgeInsets.symmetric(horizontal: 10),
                     child: Center(
                         child: TextField(
+                      //controller: searchController,
+                      onChanged: ((text) {
+                        setState(() {
+                          search = text.toLowerCase();
+                        });
+                      }),
                       decoration: const InputDecoration(
                           border: InputBorder.none, hintText: "Search for category...", suffixIcon: Icon(Icons.search)),
                       style: Theme.of(context).textTheme.titleLarge,
@@ -43,38 +52,55 @@ class _HomeScreenCategoriesTabState extends State<HomeScreenCategoriesTab> {
   }
 
   var data = <PodcastCategory>[
-    PodcastCategory("Arts", const AssetImage("assets/images/aboat.png")),
-    PodcastCategory("Arts2", const AssetImage("assets/images/aboat.png")),
-    PodcastCategory("Arts3", const AssetImage("assets/images/aboat.png"))
+    PodcastCategory("Arts", const AssetImage("assets/icons/icon-art.png")),
+    PodcastCategory("Health & Fitness", const AssetImage("assets/icons/icon-workout.png")),
+    PodcastCategory("Business", const AssetImage("assets/icons/icon-business-deal.png")),
+    PodcastCategory("Fiction", const AssetImage("assets/icons/icon-fiction.png")),
+    PodcastCategory("Government", const AssetImage("assets/icons/icon-poll.png")),
+    PodcastCategory("History", const AssetImage("assets/icons/icon-history.png")),
+    PodcastCategory("Leisure", const AssetImage("assets/icons/icon-art.png")),
+    PodcastCategory("Music", const AssetImage("assets/icons/icon-workout.png")),
+    PodcastCategory("Science", const AssetImage("assets/icons/icon-art.png")),
+    PodcastCategory("Sports", const AssetImage("assets/icons/icon-workout.png")),
+    PodcastCategory("Technology", const AssetImage("assets/icons/icon-art.png")),
+    PodcastCategory("TV & Film", const AssetImage("assets/icons/icon-workout.png")),
+    PodcastCategory("Society & Culture", const AssetImage("assets/icons/icon-art.png")),
+    PodcastCategory("Kids & Family", const AssetImage("assets/icons/icon-workout.png")),
+    PodcastCategory("Education", const AssetImage("assets/icons/icon-art.png")),
+    PodcastCategory("Comedy", const AssetImage("assets/icons/icon-workout.png")),
   ];
 
   Widget buildCategoryList(BuildContext context) {
+    var filteredItems = data.where((element) => search == '' || element.name.toLowerCase().contains(search)).toList();
+
     return GridView.count(
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
         shrinkWrap: true,
         crossAxisCount: 2,
         semanticChildCount: data.length,
         childAspectRatio: 170 / 70,
         physics: const NeverScrollableScrollPhysics(),
-        children: List.generate(data.length, (index) {
-          if (data[index] == null) {
-            return const ListTile();
-          } else {
-            final item = data[index]!;
-            return makeCard(context, item, index);
-          }
+        children: List.generate(filteredItems.length, (index) {
+          final item = filteredItems[index];
+          return makeCard(context, item, index);
         }));
   }
 
   Widget makeCard(BuildContext context, PodcastCategory category, int index) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: SizedBox(
-          width: 170,
-          height: 70,
-          child: InkWell(
-              borderRadius: BorderRadius.circular(10),
-              onTap: () async {
-                /*await userService.UpdatePodcastVisitDate(entry.id);
+    return SizedBox(
+      width: 170,
+      height: 70,
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+              color: const Color.fromRGBO(29, 40, 58, 1.0),
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                    borderRadius: BorderRadius.circular(10),
+                    onTap: () async {
+                      /*await userService.UpdatePodcastVisitDate(entry.id);
             setState(() {});
             Navigator.push(
                 context,
@@ -85,35 +111,33 @@ class _HomeScreenCategoriesTabState extends State<HomeScreenCategoriesTab> {
                     duration: const Duration(milliseconds: 500),
                     reverseDuration: const Duration(milliseconds: 500),
                     child: PodcastDetailScreen(podcastSearchResult: entry)));*/
-              },
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                    color: const Color.fromRGBO(29, 40, 58, 1.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(
-                            child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: SizedBox(
-                                  height: 50,
-                                  width: 50,
-                                  child: Image(
-                                    image: category.image,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ))),
-                        Center(
-                            child: Padding(
-                                padding: const EdgeInsets.only(left: 10),
-                                child: Text(category.name,
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 2,
-                                    style: Theme.of(context).textTheme.labelLarge)))
-                      ],
-                    )),
-              )),
-        ));
+                    },
+                    child: Padding(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10, left: 10),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                                child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10),
+                                    child: SizedBox(
+                                      height: 50,
+                                      width: 50,
+                                      child: Image(
+                                        image: category.image,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ))),
+                            Center(
+                                child: Padding(
+                                    padding: const EdgeInsets.only(left: 10),
+                                    child: Text(category.name,
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 2,
+                                        style: Theme.of(context).textTheme.labelLarge)))
+                          ],
+                        ))),
+              ))),
+    );
   }
 }
