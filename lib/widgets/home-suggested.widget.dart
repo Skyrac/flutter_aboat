02777c58addd0a49851a188dac47c6a12f966376
Alexander Krahl <alexander.krahl@daubit.org>
2022-10-18 11:58:47 +0200

    refactor podcast sidescrollbox code

--- lib/widgets/home-suggested.widget.dart
+++ lib/widgets/home-suggested.widget.dart
@@ -1,8 +1,8 @@
 import 'package:Talkaboat/injection/injector.dart';
+import 'package:Talkaboat/models/podcasts/podcast.model.dart';
 import 'package:Talkaboat/services/repositories/podcast.repository.dart';
 import 'package:Talkaboat/services/state/state.service.dart';
 import 'package:Talkaboat/services/user/user.service.dart';
-import 'package:Talkaboat/widgets/library-preview.widget.dart';
 import 'package:Talkaboat/widgets/podcast-favorites.widget.dart';
 import 'package:Talkaboat/widgets/podcast-list.widget.dart';
 import 'package:Talkaboat/widgets/quests/quest-list.widget.dart';
@@ -41,7 +41,7 @@ class _HomeScreenSuggestedTabState extends State<HomeScreenSuggestedTab> {
       const SizedBox(height: 5),
       createTaskBar(context, 'Tasks'),
       const SizedBox(height: 20),
-      createPodcastPreviewRecentlyListed(context),
+      createSideScrollPodcasts(context, PodcastRepository.getRandomPodcast(10), title: "Recently Listened"),
       const SizedBox(height: 20),
       createFavoritesList(context),
     ];
@@ -69,44 +69,6 @@ class _HomeScreenSuggestedTabState extends State<HomeScreenSuggestedTab> {
           child: QuestListWidget(
             direction: Axis.horizontal,
           ))
-    ]);
-  }
-
-  Widget createPodcastPreviewRecentlyListed(BuildContext context) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      Padding(
-        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-        child: Text("Recently Listened", style: Theme.of(context).textTheme.titleLarge),
-      ),
-      SizedBox(
-          height: 150,
-          child: userService.podcastProposalsHomeScreen.containsKey(0)
-              ? PodcastListWidget(
-                  direction: Axis.horizontal,
-                  searchResults: userService.getProposals(0)!,
-                  checkUpdate: false,
-                )
-              : FutureBuilder(
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done) {
-                      if (snapshot.hasError) {
-                        return Center(
-                          child: Text(
-                            '${snapshot.error} occurred',
-                            style: const TextStyle(fontSize: 18),
-                          ),
-                        );
-                      } else if (snapshot.hasData && snapshot.data != null) {
-                        if (snapshot.data != null && snapshot.data!.isNotEmpty) {
-                          userService.podcastProposalsHomeScreen[0] = snapshot.data!;
-                          return PodcastListWidget(direction: Axis.horizontal, searchResults: homeState.map[0]!);
-                        }
-                      }
-                    }
-                    return const Center(child: CircularProgressIndicator());
-                  },
-                  future: PodcastRepository.getRandomPodcast(10),
-                ))
     ]);
   }
 
@@ -153,6 +115,61 @@ class _HomeScreenSuggestedTabState extends State<HomeScreenSuggestedTab> {
         )
       ]),
     );
+  }
+
+  Widget createSideScrollPodcasts(BuildContext context, Future<List<Podcast>> future,
+      {String? title, String? multiplier, void Function()? seeAllCb}) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      title != null
+          ? Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Text(title, style: Theme.of(context).textTheme.titleLarge),
+                seeAllCb != null
+                    ? InkWell(
+                        borderRadius: BorderRadius.circular(10),
+                        onTap: (() {
+                          seeAllCb();
+                        }),
+                        child: Row(
+                          children: [
+                            Text(
+                              "See All",
+                              style: Theme.of(context).textTheme.titleSmall,
+                            ),
+                            const Icon(Icons.arrow_right_alt)
+                          ],
+                        ),
+                      )
+                    : Container(),
+              ]))
+          : Container(),
+      SizedBox(
+        height: 150,
+        child: FutureBuilder(
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    '${snapshot.error} occurred',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                );
+              } else if (snapshot.hasData && snapshot.data != null) {
+                if (snapshot.data != null && snapshot.data!.isNotEmpty) {
+                  return PodcastListWidget(direction: Axis.horizontal, searchResults: snapshot.data!);
+                }
+              }
+              // TODO: display a nice text
+              return Container();
+            }
+            return const Center(child: CircularProgressIndicator());
+          },
+          future: future,
+        ),
+      )
+    ]);
   }
 
   Widget createPodcastPreviewByGenre(BuildContext context, String title, int genre) {
