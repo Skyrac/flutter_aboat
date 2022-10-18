a60afaaf32bd66732d9fff013814dbb09122bbe8
Alexander Krahl <alexander.krahl@daubit.org>
2022-10-18 15:03:07 +0200

    add hodler, receivers to categories page, fix padding on categories list, refactor Podcastlist

--- lib/widgets/home-suggested.widget.dart
+++ lib/widgets/home-suggested.widget.dart
@@ -1,11 +1,11 @@
 import 'package:Talkaboat/injection/injector.dart';
 import 'package:Talkaboat/models/podcasts/podcast-rank.model.dart';
-import 'package:Talkaboat/models/podcasts/podcast.model.dart';
 import 'package:Talkaboat/services/audio/podcast.service.dart';
 import 'package:Talkaboat/services/repositories/podcast.repository.dart';
 import 'package:Talkaboat/services/state/state.service.dart';
 import 'package:Talkaboat/services/user/user.service.dart';
 import 'package:Talkaboat/widgets/podcast-favorites.widget.dart';
+import 'package:Talkaboat/widgets/podcast-list-horizontal.widget.dart';
 import 'package:Talkaboat/widgets/podcast-list.widget.dart';
 import 'package:Talkaboat/widgets/quests/quest-list.widget.dart';
 import 'package:flutter/material.dart';
@@ -44,18 +44,24 @@ class _HomeScreenSuggestedTabState extends State<HomeScreenSuggestedTab> {
       const SizedBox(height: 5),
       createTaskBar(context, 'Tasks'),
       const SizedBox(height: 20),
-      createSideScrollPodcasts(context, PodcastRepository.getRandomPodcast(10), title: "Recently Listened"),
+      PodcastListHorizontal(future: PodcastRepository.getRandomPodcast(10), title: "Recently Listened"),
       const SizedBox(height: 20),
       createFavoritesList(context),
       const SizedBox(height: 20),
-      createSideScrollPodcasts(context, podcastService.search("", amount: 10, offset: 0, rank: PodcastRank.NewComer),
-          title: "Newcomer", multiplier: "x1.5"),
+      PodcastListHorizontal(
+          future: podcastService.search("", amount: 10, offset: 0, rank: PodcastRank.NewComer),
+          title: "Newcomer",
+          multiplier: "x1.5"),
       const SizedBox(height: 20),
-      createSideScrollPodcasts(context, podcastService.search("", amount: 10, offset: 0, rank: PodcastRank.Receiver),
-          title: "Receiver", multiplier: "x1.25"),
+      PodcastListHorizontal(
+          future: podcastService.search("", amount: 10, offset: 0, rank: PodcastRank.Receiver),
+          title: "Receiver",
+          multiplier: "x1.25"),
       const SizedBox(height: 20),
-      createSideScrollPodcasts(context, podcastService.search("", amount: 10, offset: 0, rank: PodcastRank.Hodler),
-          title: "Holder", multiplier: "x1.1"),
+      PodcastListHorizontal(
+          future: podcastService.search("", amount: 10, offset: 0, rank: PodcastRank.Hodler),
+          title: "Holder",
+          multiplier: "x1.1"),
     ];
   }
 
@@ -127,79 +133,6 @@ class _HomeScreenSuggestedTabState extends State<HomeScreenSuggestedTab> {
         )
       ]),
     );
-  }
-
-  Widget createSideScrollPodcasts(BuildContext context, Future<List<Podcast>> future,
-      {String? title, String? multiplier, void Function()? seeAllCb}) {
-    List<Widget> flame = multiplier != null
-        ? <Widget>[
-            const SizedBox(
-              width: 10,
-            ),
-            const Image(
-              image: AssetImage("assets/icons/icon_fire.png"),
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            // TODO: bottom align this text
-            Text("Reward $multiplier", style: Theme.of(context).textTheme.titleMedium)
-          ]
-        : [Container()];
-
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      title != null
-          ? Padding(
-              padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                Text(title, style: Theme.of(context).textTheme.titleLarge),
-                ...flame,
-                const Spacer(),
-                seeAllCb != null
-                    ? InkWell(
-                        borderRadius: BorderRadius.circular(10),
-                        onTap: (() {
-                          seeAllCb();
-                        }),
-                        child: Row(
-                          children: [
-                            Text(
-                              "See All",
-                              style: Theme.of(context).textTheme.titleSmall,
-                            ),
-                            const Icon(Icons.arrow_right_alt)
-                          ],
-                        ),
-                      )
-                    : Container(),
-              ]))
-          : Container(),
-      SizedBox(
-        height: 150,
-        child: FutureBuilder(
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text(
-                    '${snapshot.error} occurred',
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                );
-              } else if (snapshot.hasData && snapshot.data != null) {
-                if (snapshot.data != null && snapshot.data!.isNotEmpty) {
-                  return PodcastListWidget(direction: Axis.horizontal, searchResults: snapshot.data!);
-                }
-              }
-              // TODO: display a nice text
-              return Container();
-            }
-            return const Center(child: CircularProgressIndicator());
-          },
-          future: future,
-        ),
-      )
-    ]);
   }
 
   Widget createPodcastPreviewByGenre(BuildContext context, String title, int genre) {
---
+++ lib/widgets/podcast-list-horizontal.widget.dart
@@ -0,0 +1,100 @@
+import 'package:Talkaboat/models/podcasts/podcast.model.dart';
+import 'package:Talkaboat/widgets/podcast-list.widget.dart';
+import 'package:flutter/material.dart';
+
+class PodcastListHorizontal extends StatelessWidget {
+  const PodcastListHorizontal({this.data, this.future, this.title, this.multiplier, this.seeAllCb, Key? key})
+      : super(key: key);
+
+  final Future<List<Podcast>>? future;
+  final List<Podcast>? data;
+  final String? title;
+  final String? multiplier;
+  final void Function()? seeAllCb;
+
+  Widget? buildList(BuildContext context) {
+    assert(future != null || data != null);
+    if (future != null) {
+      return FutureBuilder(
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  '${snapshot.error} occurred',
+                  style: const TextStyle(fontSize: 18),
+                ),
+              );
+            } else if (snapshot.hasData && snapshot.data != null) {
+              if (snapshot.data != null && snapshot.data!.isNotEmpty) {
+                return PodcastListWidget(direction: Axis.horizontal, searchResults: snapshot.data!);
+              }
+            }
+            // TODO: display a nice text
+            return Container();
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+        future: future!,
+      );
+    }
+
+    if (data != null) {
+      return PodcastListWidget(direction: Axis.horizontal, searchResults: data!);
+    }
+
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> flame = multiplier != null
+        ? <Widget>[
+            const SizedBox(
+              width: 10,
+            ),
+            const Image(
+              image: AssetImage("assets/icons/icon_fire.png"),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            // TODO: bottom align this text
+            Text("Reward $multiplier", style: Theme.of(context).textTheme.titleMedium)
+          ]
+        : [Container()];
+
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      title != null
+          ? Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Text(title!, style: Theme.of(context).textTheme.titleLarge),
+                ...flame,
+                const Spacer(),
+                seeAllCb != null
+                    ? InkWell(
+                        borderRadius: BorderRadius.circular(10),
+                        onTap: (() {
+                          seeAllCb!();
+                        }),
+                        child: Row(
+                          children: [
+                            Text(
+                              "See All",
+                              style: Theme.of(context).textTheme.titleSmall,
+                            ),
+                            const Icon(Icons.arrow_right_alt)
+                          ],
+                        ),
+                      )
+                    : Container(),
+              ]))
+          : Container(),
+      SizedBox(
+        height: 150,
+        child: buildList(context),
+      )
+    ]);
+  }
+}
