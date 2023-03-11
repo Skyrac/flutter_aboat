f4e407db00d3b0ac2db1e5653687136d8f3db1cc
Fabian Reichenberg <fabian.reichenberg@aboat-entertainment.com>
2023-03-11 17:53:32 +0100

    update transparency request iOS

--- lib/main.dart
+++ lib/main.dart
@@ -70,53 +70,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final userService = getIt<UserService>();
-  String _authStatus = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlugin();
-  }
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlugin() async {
-    final TrackingStatus status =
-    await AppTrackingTransparency.trackingAuthorizationStatus;
-    setState(() => _authStatus = '$status');
-    // If the system can show an authorization request dialog
-    if (status == TrackingStatus.notDetermined) {
-      // Show a custom explainer dialog before the system dialog
-      await showCustomTrackingDialog(context);
-      // Wait for dialog popping animation
-      await Future.delayed(const Duration(milliseconds: 200));
-      // Request system's tracking authorization dialog
-      final TrackingStatus status =
-      await AppTrackingTransparency.requestTrackingAuthorization();
-      setState(() => _authStatus = '$status');
-    }
-
-    final uuid = await AppTrackingTransparency.getAdvertisingIdentifier();
-    AdManager.setAdvertismentIdentifier(uuid);
-    print("UUID: $uuid");
-  }
-
-  Future<void> showCustomTrackingDialog(BuildContext context) async =>
-      await showDialog<void>(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: const Text('Dear User'),
-          content: const Text(
-            'We care about your privacy and data security. We keep this app free by showing ads. '
-                'Can we continue to use your data to tailor ads for you?\n\nYou can change your choice anytime in the app settings. '
-                'Our partners will collect data and use a unique identifier on your device to show you ads.',
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('Continue'),
-            ),
-          ],
-        ),
-      );
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
--- lib/screens/root.screen.dart
+++ lib/screens/root.screen.dart
@@ -3,7 +3,11 @@ import 'package:Talkaboat/screens/app.screen.dart';
 import 'package:Talkaboat/screens/login.screen.dart';
 import 'package:Talkaboat/screens/onboarding/onboarding.screen.dart';
 import 'package:Talkaboat/services/user/user.service.dart';
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
+
+import '../services/ads/ad-manager.service.dart';
 
 class RootScreen extends StatefulWidget {
   const RootScreen({Key? key}) : super(key: key);
@@ -14,6 +18,47 @@ class RootScreen extends StatefulWidget {
 
 class _RootScreenState extends State<RootScreen> {
   final userService = getIt<UserService>();
+  String _authStatus = 'Unknown';
+
+  @override
+  void initState() async {
+    WidgetsBinding.instance.addPostFrameCallback((_) async => await initPlugin());
+    super.initState();
+  }
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlugin() async {
+    final TrackingStatus status =
+    await AppTrackingTransparency.trackingAuthorizationStatus;
+
+    setState(() => _authStatus = '$status');
+    debugPrint("$status: ${status.name}");
+    // If the system can show an authorization request dialog
+    if (status == TrackingStatus.notDetermined) {
+      // Show a custom explainer dialog before the system dialog
+      await showDialog<void>(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Dear User'),
+            content: Text(AppLocalizations.of(context)!.appTrackingCustomText),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Continue'),
+              ),
+            ],
+          ));
+      // Wait for dialog popping animation
+      await Future.delayed(const Duration(milliseconds: 200));
+      // Request system's tracking authorization dialog
+      final TrackingStatus status =
+      await AppTrackingTransparency.requestTrackingAuthorization();
+      setState(() => _authStatus = '$status');
+    }
+
+    final uuid = await AppTrackingTransparency.getAdvertisingIdentifier();
+    AdManager.setAdvertismentIdentifier(uuid);
+    print("UUID: $uuid");
+  }
 
   @override
   Widget build(BuildContext context) {
